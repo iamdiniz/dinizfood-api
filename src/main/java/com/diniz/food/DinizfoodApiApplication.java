b505de721c8a5881abd8e20b2bfc63cd9c7bc777
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-05-23 18:25:12 -0300

    Project created

---
+++ src/main/java/com/diniz/food/DinizfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.diniz.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinizfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinizfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diniz/food/DinizfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diniz.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinizfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

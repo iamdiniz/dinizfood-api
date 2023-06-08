19f215135b6b1a7ccb9a366692ad4e44848066e9
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-06-08 15:29:22 -0300

    Cozinha & Restaurante entities

---
+++ src/main/java/com/diniz/food/domain/model/Cozinha.java
@@ -0,0 +1,48 @@
+package com.diniz.food.domain.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cozinha {
+
+	@Id
+	private Long id;
+	
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cozinha other = (Cozinha) obj;
+		return Objects.equals(id, other.id);
+	}
+}

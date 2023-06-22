f7ef0dd6733a43e69cd44334c2cc66190d36d8cb
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-06-22 18:08:20 -0300

    Implementação de Post e Put com ResponseEntity

--- src/main/java/com/diniz/food/api/controller/CozinhaController.java
+++ src/main/java/com/diniz/food/api/controller/CozinhaController.java
@@ -2,12 +2,18 @@ package com.diniz.food.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diniz.food.api.model.CozinhasXmlWrapper;
@@ -39,8 +45,29 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+			@RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinhaAtual != null) {
+//			cozinhaAtual.setNome(cozinha.getNome()); é a mesma coisa de fazer isso
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			
+			cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 }

b910fad89f6ed62248a7e4f99a6abd6277d38dec
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-07-27 16:48:08 -0300

    Criação de Pedido, item de pedido e migrações

---
+++ src/main/java/com/diniz/food/domain/model/ItemPedido.java
@@ -0,0 +1,37 @@
+package com.diniz.food.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer quantidade;
+	private BigDecimal precoUnitario;
+	private BigDecimal precoTotal;
+	private String observacao;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Pedido pedido;
+	
+}
---
+++ src/main/java/com/diniz/food/domain/model/StatusPedido.java
@@ -0,0 +1,10 @@
+package com.diniz.food.domain.model;
+
+public enum StatusPedido {
+
+	CRIADO,
+	CONFIRMADO,
+	ENTREGUE,
+	CANCELADO;
+	
+}

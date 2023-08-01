621e678fb1e7d0bc5c56b077a652826abcf92e8e
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-08-01 16:17:02 -0300

    Utilizando o padrão no corpo das respostas

--- src/main/java/com/diniz/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/diniz/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -26,13 +26,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
 		
-//		Problem problem = Problem.builder()
-//				.status(status.value())
-//				.type("https://dinizfood.com.br/entidade-nao-encontrada")
-//				.title("Entidade não encontrada")
-//				.detail(ex.getMessage())
-//				.build();
-		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(),
 				status, request);
 	}
@@ -41,14 +34,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<?> handleEntidadeEmUsoException(
 			EntidadeEmUsoException ex, WebRequest request) {
 		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-				HttpStatus.CONFLICT, request);
+		HttpStatus status = HttpStatus.CONFLICT;
+		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+		String detail = ex.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request) {
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-				HttpStatus.BAD_REQUEST, request);
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		String detail = ex.getMessage();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
 	}
 	
 	@Override

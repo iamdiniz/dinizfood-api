37bc7291242de49ada9df7ef2859a0f1009afc9d
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-08-10 12:39:42 -0300

    Tratando exception de parâmetro de URL inválido

--- src/main/java/com/diniz/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/diniz/food/api/exceptionhandler/ProblemType.java
@@ -8,7 +8,8 @@ public enum ProblemType {
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreesível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "O parâmetro está inválido");
 	
 	private String title;
 	private String uri;

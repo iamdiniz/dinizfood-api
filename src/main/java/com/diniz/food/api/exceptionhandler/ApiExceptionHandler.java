a6c8707a7c2a58f2a9b47b8fc569eaf3df02a0a7
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-08-14 15:28:13 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/diniz/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/diniz/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -31,6 +33,30 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL
 		= "Ocorreu um erro interno inesperado no sistema. Tente novamente e se o problema"
 				+ " persistir, entre em contato com o administrador do sistema";
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+		String detail = "Um ou mais campos estão inválidos. Faça o preenchiment correto e tente novamente.";
+		
+		BindingResult bindingResult = ex.getBindingResult();
+		
+		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+					.map(fieldError -> Problem.Field.builder()
+							.name(fieldError.getField())
+							.userMessage(fieldError.getDefaultMessage())
+							.build())
+					.collect(Collectors.toList());
+		
+		Problem problem = createProblemBuilder((HttpStatus) status, problemType, detail)
+				.userMessage(detail)
+				.fields(problemFields)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
